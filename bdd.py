d933fa8431480fbf62848281f1c562e7572650c2
edouard333 <edouard128@hotmail.com>
2023-06-16 10:54:35 +0200

    N'utilise plus MySQL mais SQLite.

--- bdd.py
+++ bdd.py
@@ -5,10 +5,14 @@
 # Bien si on veut ajouter des infos de QC pour ProQC ! :)
 
 # == IMPORTS ==
-import mysql.connector
+import datetime
+import os
+import sqlite3
 
-cursor = None
-conn = None
+# Pour SQLite :
+fichier = 'data.db'
+db = None
+cur = None
 
 
 # == FONCTIONS ==
@@ -16,22 +20,144 @@ def connect() -> None:
     """
     Connexion à la base de données.
     """
-    global cursor, conn
-    conn = mysql.connector.connect(host='localhost', user='root', password='', database='rapport_qc')
-    cursor = conn.cursor()
+    global db, cur, fichier
+
+    new = False
+
+    if not os.path.exists(fichier):
+        new = True
+
+    db = sqlite3.connect(fichier, isolation_level=None)
+    cur = db.cursor()
+
+    # Si la DB n'existait pas, on crée les tables et les données de base :
+    if new:
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS liste_cadence(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cadence TEXT UNIQUE
+        )
+        ''')
+
+        cur.execute('INSERT INTO liste_cadence' +
+                    '(cadence)' +
+                    'VALUES' +
+                    '("24"),("25"),("23.976")')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS liste_codec(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            codec TEXT UNIQUE
+        )
+        ''')
+
+        # J'ai un doute pour les codecs :
+        cur.execute('INSERT INTO liste_codec' +
+                    '(codec)' +
+                    'VALUES' +
+                    '("H264"),("Apple Pro Res 422HQ"),("Apple Pro Res 444")')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS liste_ratio(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ratio TEXT UNIQUE
+        )
+        ''')
+
+        cur.execute('INSERT INTO liste_ratio' +
+                    '(ratio)' +
+                    'VALUES' +
+                    '("2.40"),("2.39"),("2.35"),("2.00"),("1.85"),("1.77"),("1.66"),("1.33")')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS liste_resolution(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            resolution TEXT UNIQUE
+        )
+        ''')
+
+        cur.execute('INSERT INTO liste_codec' +
+                    '(codec)' +
+                    'VALUES' +
+                    '("1920x1080"),("3840x2160")')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_qc(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fichier TEXT,
+            date_creation TEXT
+        )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_comparaison(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fichier1_nom TEXT,
+            fichier2_nom TEXT,
+            date_creation TEXT,
+            date_commencement TEXT,
+            date_fin TEXT,
+            fichier1_duree_image INTEGER,
+            fichier2_duree_image INTEGER,
+            fichier1_start_timecode TEXT,
+            fichier2_start_timecode TEXT,
+            fichier1_framerate TEXT,
+            fichier2_framerate TEXT,
+            fichier2_offset_image TEXT
+        )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_comparaison_difference(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_rapport_comparaison INTEGER,
+            fichier2_timecode_in TEXT,
+            fichier2_timecode_in TEXT,
+        )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_qc_informations(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_rapport_qc INTEGER,
+            duree INTEGER,
+            start_timecode TEXT,
+            framerate TEXT,
+            ratio TEXT,
+        )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_qc_remarque(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_rapport_qc INTEGER,
+            timecode_in INTEGER,
+            timecode_out INTEGER,
+            remarque TEXT,
+            options TEXT,
+            echelle INTEGER
+        )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS rapport_qc_message(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_rapport INTEGER,
+            message TEXT
+        )
+        ''')
 
 
 def insert(table: str, colonnes: str, valeurs: str) -> None:
     """
-    Ajouter des donnees dans la base de données.
+    Ajouter des données dans la base de données.
 
     :param str table: Nom de la table
     :param str colonnes: La liste des colonnes.
     :param str valeurs: La liste des valeurs.
     """
-    global cursor
-    cursor.execute('INSERT INTO ' + table + ' (' + colonnes + ') VALUES (' + valeurs + ')')
-    conn.commit()
+    global cur
+    cur.execute('INSERT INTO ' + table + ' (' + colonnes + ') VALUES (' + valeurs + ')')
 
 
 def addRapport(fichier: str) -> int:
@@ -39,20 +165,22 @@ def addRapport(fichier: str) -> int:
     Ajoute un rapport à la base de données.
 
     :param str fichier: Le fichier à analyser.
+    :returns: ID du rapport créé.
     """
-    global cursor
-    cursor.execute('INSERT INTO rapport_qc (fichier, date_creation) VALUES ("' + fichier + '", NOW())')
-    conn.commit()
-    cursor.execute('SELECT id FROM rapport_qc WHERE fichier = "' + fichier + '" ORDER BY id DESC')
-    # print(cursor.fetchall()[0])
-    return int(cursor.fetchall()[0][0])
+    global cur
+    cur.execute('INSERT INTO rapport_qc (fichier, date_creation) VALUES ("' + fichier + '", "' + str(
+        datetime.datetime.now()) + '")')
+    cur.execute('SELECT id FROM rapport_qc WHERE fichier = "' + fichier + '" ORDER BY id DESC')
+    return int(cur.fetchall()[0][0])
 
 
 def addMessage(id_rapport: int, message: str) -> None:
-    global cursor
-    cursor.execute(
+    """
+    Ajout un message à la base de données.
+    """
+    global cur
+    cur.execute(
         'INSERT INTO rapport_qc_message (id_rapport, message) VALUES (' + str(id_rapport) + ', "' + message + '")')
-    conn.commit()
 
 
 def update(table: str, valeurs: str, condition: str = '*') -> None:
@@ -63,8 +191,8 @@ def update(table: str, valeurs: str, condition: str = '*') -> None:
     :param str valeurs: Colonne et valeur.
     :param str condition: Condition SQL (optionnel).
     """
-    global cursor
-    cursor.execute('UPDATE ' + table + ' SET ' + valeurs + ' WHERE ' + condition)
+    global cur
+    cur.execute('UPDATE ' + table + ' SET ' + valeurs + ' WHERE ' + condition)
 
 
 def select(table: str, selection: str = '*', condition: str = ''):
@@ -75,16 +203,17 @@ def select(table: str, selection: str = '*', condition: str = ''):
     :param str selection:
     :param str condition:
     """
-    global cursor
-    cursor.execute('SELECT ' + selection + ' FROM ' + table + condition)
-    return cursor.fetchall()
+    global cur
+    cur.execute('SELECT ' + selection + ' FROM ' + table + condition)
+    return cur.fetchall()
 
 
 def convertTab(fetch):
     """
     Convertir un résultat select en un beau tableau exploitable.
 
-    :param fetch:
+    :param fetch: Le contenu du fetch.
+    :returns: Un tableau avec une colonne.
     """
     tab = []
     for row in fetch:
@@ -100,18 +229,16 @@ def getID(table: str, condition: str) -> str:
     :param str condition : Condition sur la table.
     :return: ID.
     """
-    global cursor
-    cursor.execute('SELECT id FROM ' + table + " WHERE " + condition)
-    for valeur in cursor.fetchall():
+    global cur
+    cur.execute('SELECT id FROM ' + table + " WHERE " + condition)
+    for valeur in cur.fetchall():
         return format(valeur[0])
-
-    # Dernier id?
-    # emp_no = cursor.lastrowid
 
 
 def close() -> None:
     """
     On se déconnecte de la basse de données.
     """
-    conn.close()
-    cursor.close()
+    global cur, db
+    cur.close()
+    db.close()
